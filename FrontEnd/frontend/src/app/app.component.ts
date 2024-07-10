e76ec4c36c9d56d5e500f06257554be7024c6f1b
Victor Assuncao <Victor.a.schumann@gmail.com>
2024-07-10 14:17:44 -0300

    Create Header Component

--- FrontEnd/frontend/src/app/app.component.ts
+++ FrontEnd/frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from "./components/template/header/header.component";
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MatToolbarModule],
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
