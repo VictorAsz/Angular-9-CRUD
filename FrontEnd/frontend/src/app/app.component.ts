a791a1759fb1876ff60c4088bfcd159d0d31d55b
Victor Assuncao <Victor.a.schumann@gmail.com>
2024-07-10 14:28:25 -0300

    Footer Component Create

--- FrontEnd/frontend/src/app/app.component.ts
+++ FrontEnd/frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/template/header/header.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
 import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MatToolbarModule],
+  imports: [RouterOutlet, HeaderComponent, MatToolbarModule, FooterComponent],
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
