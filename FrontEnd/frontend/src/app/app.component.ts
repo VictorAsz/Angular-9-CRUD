6d369c02a3e5c9b123f86425923d2f18a73f4a3c
Victor Assuncao <Victor.a.schumann@gmail.com>
2024-07-11 11:43:12 -0300

    Create Module

--- FrontEnd/frontend/src/app/app.component.ts
+++ FrontEnd/frontend/src/app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/template/header/header.component";
-import { FooterComponent } from "./components/template/footer/footer.component";
-import { NavComponent } from "./components/template/nav/nav.component";
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule} from'@angular/material/sidenav'
-import { MatList } from'@angular/material/list'
+
+
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MatToolbarModule, FooterComponent, MatSidenavModule, NavComponent , MatList],
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
--- FrontEnd/frontend/src/app/components/template/footer/footer.component.ts
+++ FrontEnd/frontend/src/app/components/template/footer/footer.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
+
+
 @Component({
   selector: 'app-footer',
-  standalone: true,
-  imports: [MatToolbarModule],
+  standalone: false,
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
